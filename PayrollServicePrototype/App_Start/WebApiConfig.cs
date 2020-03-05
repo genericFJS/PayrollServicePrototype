977554383fcff91ac49bc01d3f08abd83cf7f84e
genericFJS <freejailsymphonic@yahoo.de>
2020-03-05 11:34:38 +0100

    Add JSON and DI config

--- PayrollServicePrototype/App_Start/WebApiConfig.cs
+++ PayrollServicePrototype/App_Start/WebApiConfig.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using PayrollServicePrototype.Util;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using Unity;
 
 namespace PayrollServicePrototype
 {
@@ -10,6 +10,14 @@ namespace PayrollServicePrototype
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            // JSON Default
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            // Dependency Injection Container
+            var container = new UnityContainer();
+            //container.RegisterInstance <...> (...);
+            config.DependencyResolver = new UnityResolver(container);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
