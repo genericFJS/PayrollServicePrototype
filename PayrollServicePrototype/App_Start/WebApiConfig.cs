3d72be6fe822c73891bd3ebf5a8a42fdbe65e95d
genericFJS <freejailsymphonic@yahoo.de>
2020-04-09 15:46:11 +0200

    Add CountryProvider singleton

--- PayrollServicePrototype/App_Start/WebApiConfig.cs
+++ PayrollServicePrototype/App_Start/WebApiConfig.cs
@@ -1,7 +1,6 @@
 ﻿using PayrollServicePrototype.Models;
 using PayrollServicePrototype.Util;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Headers;
 using System.Web.Http;
 using Unity;
@@ -17,30 +16,28 @@ namespace PayrollServicePrototype
             // JSON Default
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
-            var countries = new List<Country>()
-            {
-                new Country("SPA", new List<Tax>()
+            var countryProvider = CountryProvider.Instance;
+            countryProvider.Countries.Add(new Country("SPA", new List<Tax>()
                 {
                     new ProgressiveTax(0.25, 600, 0.4), new ProgressiveTax(0.07, 500, 0.08), new FlatTax(0.04)
-                }),
-                new Country("ITA", new List<Tax>()
+                }));
+            countryProvider.Countries.Add(new Country("ITA", new List<Tax>()
                 {
                     new FlatTax(0.25), new FlatTax(0.0919)
-                }),
-                new Country("DEU", new List<Tax>()
+                }));
+            countryProvider.Countries.Add(new Country("DEU", new List<Tax>()
                 {
                     new ProgressiveTax(0.25, 400, 0.32), new FlatTax(0.02)
-                })
-            };
+                }));
 
             // Dependency Injection Container
             var container = new UnityContainer();
             container.RegisterInstance<IEmployeeRepository>(new EmployeeRepository(new List<Employee>()
             {
-                new Employee(countries.Where(country => country.CountryCode == "DEU").FirstOrDefault(), 20, 10), // from description
-                new Employee(countries.Where(country => country.CountryCode == "DEU").FirstOrDefault(), 35, 30), // example
-                new Employee(countries.Where(country => country.CountryCode == "SPA").FirstOrDefault(), 35, 30), // example
-                new Employee(countries.Where(country => country.CountryCode == "ITA").FirstOrDefault(), 35, 30), // example
+                new Employee(countryProvider.CountryByCode("DEU"), 20, 10), // from description
+                new Employee(countryProvider.CountryByCode("DEU"), 35, 30), // example
+                new Employee(countryProvider.CountryByCode("SPA"), 35, 30), // example
+                new Employee(countryProvider.CountryByCode("ITA"), 35, 30), // example
             }));
             config.DependencyResolver = new UnityResolver(container);
 
---
+++ PayrollServicePrototype/Models/CountryProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PayrollServicePrototype.Models
+{
+    public class CountryProvider
+    {
+        private static readonly Lazy<CountryProvider> lazy = new Lazy<CountryProvider>(() => new CountryProvider());
+
+        public static CountryProvider Instance { get { return lazy.Value; } }
+
+        public List<Country> Countries { get; set; } = new List<Country>();
+
+        private CountryProvider()
+        {
+        }
+
+        public Country CountryByCode(string code)
+        {
+            return Countries.Where(country => country.CountryCode == code).FirstOrDefault();
+        }
+    }
+}
